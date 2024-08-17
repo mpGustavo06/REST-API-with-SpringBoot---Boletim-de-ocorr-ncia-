5432ca03140ca044c8c8d80b675c462f824d90f1
mpGustavo06 <myth.gustavo06@gmail.com>
2024-08-17 18:30:47 -0300

    SNAPSHOT 0.0.1

--- delegacia-web-api/src/main/java/com/br/utfpr/tsi/delegacia/web/api/MainApp.java
+++ delegacia-web-api/src/main/java/com/br/utfpr/tsi/delegacia/web/api/MainApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DelegaciaWebApiApplication {
+public class MainApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DelegaciaWebApiApplication.class, args);
+		SpringApplication.run(MainApp.class, args);
 	}
 
 }
--- delegacia-web-api/src/main/java/com/br/utfpr/tsi/delegacia/web/api/model/BoletimFurto.java
+++ delegacia-web-api/src/main/java/com/br/utfpr/tsi/delegacia/web/api/model/BoletimFurto.java
@@ -1,29 +1,19 @@
 package com.br.utfpr.tsi.delegacia.web.api.model;
 
-import java.time.LocalDate;
-import java.util.List;
-
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
 public class BoletimFurto {
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private String id;
+	private String identificador;
 	
-	private LocalDate dataOcorrido;
+	private String dataOcorrido;
 	private String periodoOcorrido;
-	private Endereco localOcorrido;
-	private Veiculo veiculoFurtado;
-	private List<Envolvido> envolvidos;
-	private final String crime = "Furto de veiculo -  art. 155";
-	
-	public BoletimFurto() {	}
-	
+	private String envolvidos;
+	private String localOcorrido;
+	private String veiculoFurtado;
 }
