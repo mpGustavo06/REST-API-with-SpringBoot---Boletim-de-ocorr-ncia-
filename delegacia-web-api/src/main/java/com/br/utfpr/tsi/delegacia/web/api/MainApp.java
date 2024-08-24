67e29cb8af75d272208e72e69f5e73cf2dc76d74
Gustavo Matheus Pauvels <myth.gustavo06@gmail.com>
2024-08-24 19:34:14 -0300

    VERSION 1.0

--- delegacia-web-api/src/main/java/com/br/utfpr/tsi/delegacia/web/api/MainApp.java
+++ delegacia-web-api/src/main/java/com/br/utfpr/tsi/delegacia/web/api/MainApp.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.br.utfpr.tsi.delegacia.web.api.controller.CSVLeitor;
 
 @SpringBootApplication
 @ComponentScan({"com.br.utfpr.tsi.delegacia.web.api"})
